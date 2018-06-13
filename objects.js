a6ea2949bab3c53a125920f59f4eb603f43c20dd
Manisha Bhalesha <mbhalesha@gmail.com>
2018-06-13 21:09:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,3 +10,22 @@ meals['breakfast']
 meals[firstMeal]
 meals.firstMeal
 
+var meals = {
+  breakfast: 'oatmeal',
+  lunch: 'burrito',
+  dinner: 'steak'
+}
+
+meals.snack = 'yogurt';
+
+meal.snack
+meals['snack']
+meals.lunch
+
+meals['second breakfast'] = 'bagel'
+
+var sweetMeal = 'dessert'
+meals[sweetMeal] = 'cake'
+meals.dessert
+meals[sweetMeal]
+
